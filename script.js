23073551a7df2d49123faec72fe35b35ed3796b3
vergicore <147555888+vergicore@users.noreply.github.com>
2023-12-14 21:44:30 +0000

    learned about DOM

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ let img = document.querySelector("img");
 
 
 // 2. Remove the comment backslashes from the code below. What happened?
-// paragraph.innerHTML = "Here's a photo of a bear.";
-// img.src = "https://bearwise.org/wp-content/uploads/2022/09/the-over-the-shoulder-pose-scaled-736x414.jpg"; 
+paragraph.innerHTML = "Here's a photo of a bear.";
+img.src = "https://bearwise.org/wp-content/uploads/2022/09/the-over-the-shoulder-pose-scaled-736x414.jpg"; 
--- storingElements.js
+++ storingElements.js
@@ -1,24 +1,24 @@
 // CODE ALONG
 // 1. Create a variable called `painting1` that stores the html element with the class `painting-1` using querySelector.
-
+let painting1 = document.querySelector(".painting-1");
 
 
 // CODE SOLO
 // 2. Create a variable called `painting2` that stores the html element with the class `painting-2` using querySelector.
-
+let painting2 = document.querySelector(".painting-2");
 // 3. Create a variable called `painting3` that stores the html element with the class `painting-3` using querySelector.
-
+let painting3 = document.querySelector(".painting-3");
 // 4. Create a variable called `painting4` that stores the html element with the class `painting-4` using querySelector.
-
+let painting4 = document.querySelector(".painting-4");
 // 5. Create a variable called `title` that stores the title tag with using querySelector.
-
+let title = document.querySelector("title");
 
 
 
 
 // 6. Once tasks 1-5 are complete, uncomment the code below.
-// painting1.style.width = "500px";
-// painting2.style.animation = "mynewmove 10s infinite";
-// painting3.style.animation = "dance 10s infinite";
-// painting4.style.border = " 10px solid #0fd623";
-// title.innerHTML = "MUHAHAHA!";
+painting1.style.width = "500px";
+painting2.style.animation = "mynewmove 10s infinite";
+painting3.style.animation = "dance 10s infinite";
+painting4.style.border = " 10px solid #0fd623";
+title.innerHTML = "MUHAHAHA!";
